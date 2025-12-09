b8030157b11e72d7078f9108276b39ace72ba416
Jair Marques <jairsmarques1126@gmail.com>
2025-12-09 17:25:34 -0300

    resolvendo o exercício da aula sobre vetores(problema negativos)

---
+++ exercicio-secao10-vetores-problema-negativos/src/application/Program.java
@@ -0,0 +1,37 @@
+/*
+ * Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros e 
+ * armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos.
+ * 
+ */
+
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] vect = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextInt();
+		}
+
+		System.out.println("NÚMEROS NEGATIVOS:");
+		for (int i = 0; i < n; i++) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+
+		}
+
+		sc.close();
+
+	}
+
+}
